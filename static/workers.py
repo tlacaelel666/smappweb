651f366d8d53e211909df28a1c8f0d4805daf6c7
jako <jakocrazykings@gmail.com>
2024-09-04 20:11:40 -0600

    workers tasks

--- static/workers.py
+++ static/workers.py
@@ -1,6 +1,21 @@
 from celery import Celery
 from flask import Flask
 
+app = celery('workers', broker='amqp://guest:guest@localhost//')
+
+@app.task('/convert', methods=['POST'])
+def convert():
+     url = request.form['url']
+    ydl_opts = {'format':'bestaudio'}
+    with YouTubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=False)
+        mp3_file = info['title']
+        yt = YouTube(url)
+        stream = yt.streams.filter(only_audio=True).first()
+        stream.download('Downloads')
+        file_name = f"{mp3_file}.mp3"
+        return jsonify({'message':f"File download: {file_name}"})
+
 def make_celery(app):
     celery = Celery(
         app.import_name,
@@ -14,10 +29,6 @@ def create_app():
     app = Flask(__name__)
     app.config.update(
         CELERY_BROKER_URL='redis://localhost:6379/0',
-        CELERY_RESULT_BACKEND='redis://localhost:6379/0'
-    )
-
+        CELERY_RESULT_BACKEND='redis://localhost:6379/0')
     return app
-
-app = create_app()
-celery = make_celery(app)
+    
