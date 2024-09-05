9e7690ce1ca0544b5928b1d74a8da9824794a979
jako <jakocrazykings@gmail.com>
2024-09-04 19:59:05 -0600

    LOGICA DE CONVERSION

--- static/app.py
+++ static/app.py
@@ -1,29 +1,34 @@
-from doctest import OutputChecker
-import sys
 from pytube import YouTube
 import os
-from pydub import AudioSegmrnt
-import app
 from flask import Flask, redirect, url_for,
 render_template, request, send_from_directory,
 jsonify
 
 app = flask(__name__)
-DOWNLOAD_FODER = 'downloads'
-if not os.path.exists(DOWNLOAD_FOLDER):
-os.makedirs(DOWNLOAD_FOLDER) 
 
 @app.route('/')
 def index():
- return render_template('index.html')
+    return render_template('index.html')
 
-def create_app():
-    app = flask(__name__)
-
-    app.config['FLASK_APP'] = 'flask run'
-
-    return app
+@app.route('/convert', methods=['POST'])
+def convert():
+    url = request.form['url']
+    ydl_opts = {'format':'bestaudio'}
+    with YouTubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=False)
+        mp3_file = info['title']
+        yt = YouTube(url)
+        stream = yt.streams.filter(only_audio=True).first()
+        stream.download('Downloads')
+        file_name = f"{mp3_file}.mp3"
+        return jsonify({'message':f"File download: {file_name}"})
+    
+@app.route('/download/<file_name>')
+def download(file_name):
+    return send_from_directory('Downloads', file_name)
+        Downloads = 'DOWNLOAD_FOLDER'
+if not os.path.exists(DOWNLOAD_FOLDER):
+os.makedirs(DOWNLOAD_FOLDER) 
 
 if __name__ == '__main__':
-    app = create_app()
-    app.run(host=debug=True)
+    app.run(debug=True)
